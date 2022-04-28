36cf18ac87352b24091485e09f5d1270e6f71dbb
Gichure Maina <76261570+KyleGichez@users.noreply.github.com>
2022-04-28 22:50:27 +0300

    04.28.22

---
+++ static/form.js
@@ -0,0 +1,31 @@
+//Prevents the form from submitting
+const button = () => {
+    document.getElementById("submit");
+    button.addEventListener('click',(e) =>{
+        e.preventDefault();
+    });
+};
+
+//Function to store the toggle password and password variables
+const togglePassword =  document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+//Add an event listener to the togglePassword icon
+togglePassword.addEventListener('click', (e) => {
+    //toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type",type);
+    //toggle the eye
+    this.classList.toggle("fa-eye");
+});
+
+const togglePassword1 = document.querySelector("#togglePassword1");
+const password1 = document.querySelector("#password1");
+
+togglePassword1.addEventListener('click', (e) => {
+    //toggle the type attribute
+    const type1 = password1.getAttribute("type") === "password" ? "text" : "password";
+    password1.setAttribute("type",type1);
+    //toggle the eye
+    this.classList.toggle("fa-eye");
+});
